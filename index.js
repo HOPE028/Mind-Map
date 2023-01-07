03e101edb0ab23262fcf20579979446a3f785376
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-01-06 23:43:14 -0600

    Random Placement Added

---
+++ index.js
@@ -0,0 +1,62 @@
+;(() => {
+  'use strict'
+  const TRIES_PER_BOX = 50
+  const randUint = (range) => (Math.random() * range) | 0
+  const placing = [...document.querySelectorAll('.node')].map((el) =>
+    Bounds(el, 5)
+  )
+  const fitted = []
+  const areaToFit = Bounds()
+  var maxTries = TRIES_PER_BOX * placing.length
+  while (placing.length && maxTries > 0) {
+    let i = 0
+    while (i < placing.length) {
+      const box = placing[i]
+      box.moveTo(randUint(areaToFit.w - box.w), randUint(areaToFit.h - box.h))
+      if (fitted.every((placed) => !placed.overlaps(box))) {
+        fitted.push(placing.splice(i--, 1)[0].placeElement())
+      } else {
+        maxTries--
+      }
+      i++
+    }
+  }
+  function Bounds(el, pad = 0) {
+    const box = el?.getBoundingClientRect() ?? {
+      left: innerWidth * 0.1,
+      top: innerHeight * 0.1,
+      right: innerWidth * 0.9,
+      bottom: innerHeight * 0.9,
+      width: innerWidth * 0.9,
+      height: innerHeight * 0.9,
+    }
+    return {
+      l: box.left - pad,
+      t: box.top - pad,
+      r: box.right + pad,
+      b: box.bottom + pad,
+      w: box.width + pad * 2,
+      h: box.height + pad * 2,
+      overlaps(bounds) {
+        return !(
+          this.l > bounds.r ||
+          this.r < bounds.l ||
+          this.t > bounds.b ||
+          this.b < bounds.t
+        )
+      },
+      moveTo(x, y) {
+        this.r = (this.l = x) + this.w
+        this.b = (this.t = y) + this.h
+        return this
+      },
+      placeElement() {
+        if (el) {
+          el.style.top = this.t + pad + 'px'
+          el.style.left = this.l + pad + 'px'
+        }
+        return this
+      },
+    }
+  }
+})()
